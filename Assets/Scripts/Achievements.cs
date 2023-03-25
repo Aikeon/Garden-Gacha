183eac794fa1885a46954da458591fcc24b19edf
Aikeon <91202249+Aikeon@users.noreply.github.com>
2023-03-25 13:22:32 +0100

    better ?

--- Assets/Scripts/Achievements.cs
+++ Assets/Scripts/Achievements.cs
@@ -20,9 +20,8 @@ public class Condition
     public MonoBehaviour ScriptReference{ get => scriptReference; set => scriptReference = value;}
     public MonoBehaviour scriptReference;
     // [System.NonSerialized]
-    [ReadOnly]
-    public List<string> variables = new List<string>();
-    [ListToPopup(typeof(Achievements), "variables")]
+    // [ReadOnly]
+    public List<string> variables_do_not_modify = new List<string>();
     public string variable;
 
     public float valueCheck;
@@ -55,14 +54,14 @@ public class Condition
 
     public void ResetVariables()
     {
-        variables.Clear();
+        variables_do_not_modify.Clear();
         System.Type myObjectType = scriptReference.GetType();
         System.Reflection.FieldInfo[] memberArray = myObjectType.GetFields();
         foreach (var item in memberArray)
         {
             //this if is to look only at variables and forget about methods and constructors...
             Debug.Log(item.ToString().Split(' ')[1]);
-            variables.Add(item.ToString().Split(' ')[1]);
+            variables_do_not_modify.Add(item.ToString().Split(' ')[1]);
             if (item.ToString().Split(' ')[1] == "testInt")
             {
                 var value = item.GetValue(scriptReference);
@@ -85,65 +84,29 @@ public class Achievement
     public bool done;
 }
 
-public class ReadOnlyAttribute : PropertyAttribute
- {
+// public class ReadOnlyAttribute : PropertyAttribute
+//  {
  
- }
+//  }
  
- [CustomPropertyDrawer(typeof(ReadOnlyAttribute))]
- public class ReadOnlyDrawer : PropertyDrawer
- {
-     public override float GetPropertyHeight(SerializedProperty property,
-                                             GUIContent label)
-     {
-         return EditorGUI.GetPropertyHeight(property, label, true);
-     }
+//  [CustomPropertyDrawer(typeof(ReadOnlyAttribute))]
+//  public class ReadOnlyDrawer : PropertyDrawer
+//  {
+//      public override float GetPropertyHeight(SerializedProperty property,
+//                                              GUIContent label)
+//      {
+//          return EditorGUI.GetPropertyHeight(property, label, true);
+//      }
  
-     public override void OnGUI(Rect position,
-                                SerializedProperty property,
-                                GUIContent label)
-     {
-         GUI.enabled = false;
-         EditorGUI.PropertyField(position, property, label, true);
-         GUI.enabled = true;
-     }
- }
-
-public class ListToPopupAttribute : PropertyAttribute
-{
-    public System.Type myType;
-    public string propertyName;
-
-    public ListToPopupAttribute(System.Type _myType, string _propertyName)
-    {
-        myType = _myType;
-        propertyName = _propertyName;
-    }
-}
-
-[CustomPropertyDrawer(typeof(ListToPopupAttribute))]
-public class ListToPopupDrawer : PropertyDrawer
-{
-    int selectedIndex = 0;
-
-    public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
-    {
-        ListToPopupAttribute ttb = attribute as ListToPopupAttribute;
-        List<string> stringList = null;
-
-        if (ttb.myType.GetField(ttb.propertyName) != null)
-        {
-            stringList = ttb.myType.GetField(ttb.propertyName).GetValue(ttb.myType) as List<string>;
-        }
-
-        if (stringList != null && stringList.Count != 0)
-        {
-            selectedIndex = EditorGUI.Popup(position, property.name, selectedIndex, stringList.ToArray());
-            property.stringValue = stringList[selectedIndex];
-        }
-        else EditorGUI.PropertyField(position, property, label);
-    }
-}
+//      public override void OnGUI(Rect position,
+//                                 SerializedProperty property,
+//                                 GUIContent label)
+//      {
+//          GUI.enabled = false;
+//          EditorGUI.PropertyField(position, property, label, true);
+//          GUI.enabled = true;
+//      }
+//  }
 
 public class Achievements : MonoBehaviour
 {
